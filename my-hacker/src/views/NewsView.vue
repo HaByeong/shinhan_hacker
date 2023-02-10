7a08b9a5b55887b2930ed3fc726519061e9f30a9
HaByeong <jhschris8080@naver.com>
2023-02-10 16:31:30 +0900

    My hacker

---
+++ my-hacker/src/views/NewsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>news</div>
+  <list-item :listData="list"></list-item>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue';
+
+export default {
+  components: { ListItem },
+  computed: {
+    list() {
+      return this.$store.state.list;
+    }
+  },
+  created() {
+    this.$store.dispatch('FETCH_LIST', 'news')
+  },
+}
+</script>
+
+<style>
+
+</style>
